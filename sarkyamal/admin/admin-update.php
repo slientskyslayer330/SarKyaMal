cdf4dc0770fe5294ab648fb21c8fc717d4b424cc
slientskyslayer330 <47809086+slientskyslayer330@users.noreply.github.com>
2020-01-29 15:39:35 +0630

    Add files via upload

---
+++ sarkyamal/admin/admin-update.php
@@ -0,0 +1,16 @@
+<?php 
+   include("config/connect.php"); 
+
+    $admin_id  =$_POST['admin_id'];
+    $admin_name=$_POST['admin_name'];
+	$email     =$_POST['email'];
+	$password  =$_POST['password'];
+
+	$sql="UPDATE admins SET admin_name='$admin_name', email='$email', password='$password' 
+          WHERE admin_id='$admin_id'";
+	$update=mysqli_query($conn, $sql);
+   
+   if ($update) {
+      header("location: admin-list.php");
+   }
+?>
